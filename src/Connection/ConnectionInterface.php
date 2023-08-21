c7efb74c9e59c3264e0733194224332ecea12a29
mdMandoinfo <mdMandoinfo@gmail.com>
2023-08-21 23:45:00 +0300

    ConnectionInterface.php

---
+++ src/Connection/ConnectionInterface.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of MDSocket.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    mdMandoinfo
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
